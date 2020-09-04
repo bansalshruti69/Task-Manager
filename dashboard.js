eeb458f743e9985379ea2d7c9d65a656123cd2c7
Shruti Bansal <bansalshruti69@gmail.com>
2020-09-04 18:55:50 +0530

    After completing phase2

--- dashboard.js
+++ dashboard.js
@@ -1,57 +1,86 @@
-let model = {
-    cards: null,
+const model = {
+    cardsList: null,
+    cardListId:null,
     init: function(){
-        let obj = {
-            cardTitle:"Shruti Bansal",
-            cardContent: [ {task: "Practice Project for creating a task manager webpage.", status:"In Progress"}]
-        };
-        model.cards = [];
-        model.cards.push(obj);
+        model.cardsList = [];
+        model.cardListId = 0;
     }
 };
 
-let octopus = {
+const octopus = {
     init: function(){
         model.init();
         view.init();
     },
-    addCard:function(val){
+    addCardList:function(val){
         obj = {
-            cardTitle: val,
-            cardContent:[]
+            cardListUser: val,
+            cardListContent:[],
+            display:true,
+            cardId:0
         };
-        model.cards.push(obj);
+        model.cardsList.push(obj);
+    },
+    getCardListId: function(){
+        return model.cardListId++;
+    },
+    removeCardList: function(val){
+        obj = model.cardList[val];
+        obj.display = false;
+    },
+    addCard: function(id,val){
+        obj = {
+            task: val,
+            status:"Assigned",
+            display:true
+        }
+        model.cardsList[id].cardListContent.push(obj);
+    },
+    getCardId: function(val){
+        return model.cardsList[val].cardId++;
+    },
+    removeCard: function(id1,id2){
+        model.cardsList[id1].cardListContent[id2].display = false;
     }
 };
 
-let view = {
+const view = {
     init: function(){
-        let div = document.querySelector(".card__add-list");
-        div.addEventListener('click',view.handler1);
+        div = document.querySelector(".card__add-list");
+        div.addEventListener('click',view.addAnotherListDisplay);
         buttonCancel = document.getElementById("add-another-list-cancel-button");
-        buttonCancel.addEventListener('click',view.handler2);
+        buttonCancel.addEventListener('click',view.canceladdAnotherListDisplay);
         buttonSave = document.getElementById("add-another-list-save-button");
-        buttonSave.addEventListener('click',view.handler3);
+        buttonSave.addEventListener('click',view.saveAddAnotherListDisplay);
+        accountManagementDiv = document.querySelector(".account-management-cancel");
+        accountManagementDiv.addEventListener('click',view.removeAccountManagementDiv);
+
     },
-    handler1: function(event){
+    removeAccountManagementDiv: function(event){
+        div = event.target.closest(".account-management");
+        div.remove();
+    }, 
+
+    addAnotherListDisplay: function(event){
         event.currentTarget.style.display = "none";
         input = document.getElementById("add-another-list-input");
         input.style.display="block";
     },
-    handler2: function(){
+    canceladdAnotherListDisplay: function(){
         document.getElementById("add-another-list-in").value = "";
         document.getElementById("add-another-list-input").style.display="none";
         document.querySelector(".card__add-list").style.display="";
     },
-    handler3: function(){
+    saveAddAnotherListDisplay: function(){
         val = document.getElementById("add-another-list-in").value;
-        octopus.addCard(val);
-        view.createCard(val);
-        view.handler2();
+        octopus.addCardList(val);
+        view.createCardList(val);
+        view.canceladdAnotherListDisplay();
     },
-    createCard: function(val){
+    createCardList: function(val){
         section1 = document.createElement('section');
         section1.classList.add("card");
+        section1.setAttribute("data-cardListId",octopus.getCardListId());
         //Card Title Creation
         div1 = document.createElement('div');
         div1.classList.add("card-title");
@@ -59,8 +88,10 @@ let view = {
         div2.classList.add("card-user-name");
         div2.textContent = val;
         div1.append(div2);
-        p1 = document.createElement('p');
-        p1.textContent = "...";
+        p1 = document.createElement('img');
+        p1.src="assets/images/cancel-icon.png";
+        p1.style.width = "20px";
+        p1.addEventListener('click',view.removeCardList);
         div1.append(p1);
         section1.append(div1);
         // Card Footer
@@ -71,6 +102,7 @@ let view = {
         div22 = document.createElement('div');
         div22.classList.add("add-another-card");
         div22.textContent = "+ Add another card";
+        div22.addEventListener('click',view.addAnotherCard);
         div33 = document.createElement('div');
         div33.classList.add("add-another-card");
         div33.classList.add("three-dots");
@@ -79,9 +111,88 @@ let view = {
         div11.append(div33);
         div3.append(div11);
         section1.append(div3);
+        //Hidden Add Card Functionality
+        div4 = document.createElement('div');
+        div4.style.display = "none";
+        div4.classList.add("add-another-card-input");
+        input = document.createElement('input');
+        input.type = "text";
+        input.classList.add("add-another-card-in");
+        input.placeholder="Enter a title for this card...";
+        div4.append(input);
+        div5 = document.createElement("div");
+        div5.classList.add("add-another-card-button");
+        div4.append(div5);
+        button1 = document.createElement("button");
+        button1.type = "button";
+        button1.classList.add("add-another-card-save-button");
+        button1.textContent = "Save";
+        button1.addEventListener("click",view.saveAddAnotherCard);
+        div5.append(button1);
+        button2 = document.createElement("button");
+        button2.type = "button";
+        button2.classList.add("add-another-card-cancel-button");
+        button2.textContent = "Cancel";
+        button2.addEventListener("click",view.cancelAddAnotherCard);
+        div5.append(button2);
+        div3.append(div4);
         //Card Appending
         section = document.querySelector(".card-group");
         section.insertBefore(section1,section.lastElementChild);
+    },
+    saveAddAnotherCard: function(event){
+        section = event.target.closest(".card");
+        id = section.getAttribute("data-cardListId");
+        val = section.querySelector(".add-another-card-in").value;
+        octopus.addCard(id,val);
+        view.addCard(event);
+        view.cancelAddAnotherCard(event);
+    },
+    addCard: function(event){
+        section = event.target.closest(".card");
+        id = section.getAttribute("data-cardListId");
+        val = section.querySelector(".add-another-card-in").value;
+        div1 = document.createElement('div');
+        div1.setAttribute("data-cardId",octopus.getCardId(id));
+        div1.classList.add("card-content");
+        div2 = document.createElement('div');
+        div2.classList.add("card-content-text");
+        div2.textContent = val;
+        div3 = document.createElement('div');
+        div3.classList.add("card-content-status");
+        div3.innerHTML = "Status: <span>Assigned</span>";
+        img = document.createElement('img');
+        img.src = "assets/images/edit-icon.png";
+        img.addEventListener('click',view.removeCard);
+        div1.append(div2);
+        div1.append(img);
+        div2.append(div3);
+        section.querySelector(".card-content-group").insertBefore(div1,section.querySelector(".card-footer"));
+    },
+    removeCard: function(event){
+        section = event.target.closest(".card");
+        id1 = section.getAttribute("data-cardListId");
+        div = event.target.closest(".card-content");
+        id2 = div.getAttribute("data-cardId");
+        div.remove();
+        octopus.removeCard(id1,id2);
+    },
+    cancelAddAnotherCard: function(event){
+        section = event.target.closest(".card");
+        section.querySelector(".card-footer").style.display = "";
+        section.querySelector(".add-another-card-input").style.display="none";
+        section.querySelector(".add-another-card-in").value = "";
+    },
+    removeCardList: function(event){
+        section = event.target.closest(".card");
+        id = section.getAttribute("data-cardListId");
+        section.remove();
+        octopus.removeCardList(id);
+    },
+    addAnotherCard: function(event){
+        section = event.target.closest(".card");
+        section.querySelector(".card-footer").style.display = "none";
+        section.querySelector(".add-another-card-input").style.display="";
     }
 };
 
--- user.js
+++ user.js
@@ -1,34 +1,43 @@
-let model = {
+const model = {
     users: null,
+    totalUsers:null,
     init: function(){
-        model.users = [{name:"Shruti",profile:"Product Engineer",location:"Gurugram",imageSource:"assets/images/Shruti_image.jpg"}]
+        model.users = [];
+        model.totalUsers = 0;
     }
 };
 
-let octopus = {
+const octopus = {
     init: function(){
         model.init();
         view.init();
     },
     addUser: function(obj){
         model.users.push(obj);
+    },
+    userId: function(){
+        return model.totalUsers++;
+    },
+    removeUser: function(val){
+        obj = model.users[val];
+        obj.display = false;
     }
 };
 
-let view = {
+const view = {
     init: function(){
         img = document.getElementById("add-icon-image");
-        img.addEventListener('click',view.handler1);
+        img.addEventListener('click',view.displayAddUserForm);
         cancelButton = document.getElementById("add-user-info-cancel-button");
-        cancelButton.addEventListener('click',view.handler2);
+        cancelButton.addEventListener('click',view.cancelAddUserForm);
         saveButton = document.getElementById("add-user-info-save-button");
-        saveButton.addEventListener('click',view.handler3);
+        saveButton.addEventListener('click',view.saveAddUserForm);
     },
-    handler1: function(){
+    displayAddUserForm: function(){
         document.getElementById("add-icon-image").style.display = "none";
         document.querySelector(".add-user-info").style.display = "block";
     },
-    handler2: function(){
+    cancelAddUserForm: function(){
         document.getElementById("add-icon-image").style.display = "";
         document.querySelector(".add-user-info").style.display = "none";
         document.getElementById("user-info-name").value = "";
@@ -36,16 +45,17 @@ let view = {
         document.getElementById("user-info-location").value="";
         document.getElementById("user-info-image").value="";
     },
-    handler3: function(){
+    saveAddUserForm: function(){
         obj = {
             name: document.getElementById("user-info-name").value,
             profile: document.getElementById("user-info-profile").value,
             location: document.getElementById("user-info-location").value,
-            imageSource: document.getElementById("user-info-image").value
+            imageSource: document.getElementById("user-info-image").value,
+            display: true
         }
         octopus.addUser(obj);
         view.createCard(obj);
-        view.handler2();
+        view.cancelAddUserForm();
     },
     createCard: function(obj){
         div1 = document.createElement('div');
@@ -59,11 +69,23 @@ let view = {
         div3 = document.createElement('div');
         div3.classList.add("user__profile--info");
         div3.innerHTML = obj.profile + "<br>" + obj.location;
+        button = document.createElement('button');
+        button.textContent = "delete";
+        button.classList.add("delete-user-button");
+        button.addEventListener('click',view.deleteCard);
         div1.append(img);
         div1.append(div2);
         div1.append(div3);
+        div1.append(button);
+        div1.setAttribute("data-userId",""+octopus.userId());
         div = document.querySelector(".users");
         div.insertBefore(div1,div.lastElementChild);
+    },
+    deleteCard: function(event){
+        div = event.target.closest(".user__profile");
+        userId = div.getAttribute("data-userId");
+        octopus.removeUser(userId);
+        div.remove();
     }
 };
 
